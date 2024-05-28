359bb1f9c826f62de2fce0b5f07e492747fafbc8
Wasim Afsar <wafsar@onetrust.com>
2024-05-28 15:11:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  let inputValue = useRef(null);
+  let [todoList, setTodos] = useState([]); // maintains list of added list items
+  function addTodo() {
+    console.log("Todo");
+    console.log(inputValue.current.value);
+    const updatedTodos = [...todoList, inputValue.current.value]; // adds new todo to existing todolist , you need to create a new temp array and then setTodo
+    setTodos(updatedTodos);
+    inputValue.current.value = null;
+  }
+
+  function deleteRecord(record) {
+    console.log(record);
+    record = "abcd";
+    const updatedTodos = todoList.filter((value) => value !== record); // find record that matches that name and remove that record from todo
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App-header">
+        <input type="text" ref={inputValue} name="add" id="add" />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todoList.map((response) => (
+          <li key={response}>
+            <input type="checkbox" onClick={deleteRecord} />
+            {response}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
